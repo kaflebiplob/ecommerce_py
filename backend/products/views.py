173e26b780eeb87e27a00700fab4282f89594ba8
biplob.kafle <biplobkafle21@gmail.com>
2025-10-30 20:04:12 +0545

    Filtering Implemented

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Category, Product
 from .serializers import CategorySerializer,ProductSerializer
 
 # Create your views here.
-
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -16,3 +15,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category']
+    search_fields=['name','description']
+    ordering_fields = ['price', 'created_at']
+    ordering = ['-created_at']
