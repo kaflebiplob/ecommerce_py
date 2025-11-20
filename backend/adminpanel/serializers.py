c6cddf5ef6c12488cd2050d8458e95b6dc501dc2
biplob.kafle <biplobkafle21@gmail.com>
2025-11-20 08:49:42 +0545

    updated user module

--- backend/adminpanel/serializers.py
+++ backend/adminpanel/serializers.py
@@ -108,3 +108,21 @@ class UserAdminSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id','username','email','is_superuser', 'is_staff','is_active']
         
+    def create(self, validated_data):
+        password = validated_data.pop("password");
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+        
