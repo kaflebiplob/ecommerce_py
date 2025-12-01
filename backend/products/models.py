7b633edbc0956c00b4ed697e89e921990725bd32
biplob.kafle <biplobkafle21@gmail.com>
2025-12-01 10:09:48 +0545

    backend completed

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -15,7 +16,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/',blank=True,null=True)
+    image = CloudinaryField("image", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_featured = models.BooleanField(default=False) 
